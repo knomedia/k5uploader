f9cc60014b2f1cffa0fdde8ffc597a53ae51260d
Jason Madsen <jmadsen@instructure.com>
2014-01-27 13:44:40 -0700

    remove underscore dep with simple workout

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 require.config({
   baseUrl: './lib/',
   include: ['k5uploader'],
-  shim: {
-    underscore: {
-      exports: '_'
-    }
-  },
   paths: {
-    "underscore": "../bower_components/underscore/underscore-min",
     "jquery": "../bower_components/jquery/jquery"
   }
 });
