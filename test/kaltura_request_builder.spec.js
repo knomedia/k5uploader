fb4f99c89506c0a1723576ed2dcb6162b71d2fec
Jason Madsen <jmadsen@instructure.com>
2014-02-05 16:39:39 -0700

    move request builder into uploader
    makes the interaction a bit less wonky

--- test/kaltura_request_builder.spec.js
+++ test/kaltura_request_builder.spec.js
@@ -7,10 +7,17 @@ require(['kaltura_request_builder'], function(KalturaRequestBuilder) {
   });
 
   test('holds settings, url, and file', function() {
-    expect(2);
-    requestBuilder.buildRequest('set', 'file', 'url');
-    equal(requestBuilder.getFile(), 'file');
-    equal(requestBuilder.getSettings(), 'set');
+    expect(3);
+    var session = {
+      getSession: function() {
+        return {ks: '5678909876'}
+      }
+    };
+    var file = {};
+    var xhr = requestBuilder.buildRequest(session, file);
+    equal(requestBuilder.getFile(), file);
+    equal(requestBuilder.getSettings(), session);
+    equal((xhr instanceof XMLHttpRequest), true, 'returns an xhr');
   });
 
 
