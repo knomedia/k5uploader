fa8cf6db65ac338048408d22ab4933c83fd05388
Jason Madsen <jmadsen@instructure.com>
2014-02-05 15:36:08 -0700

    more tests

---
+++ test/xml_parser.spec.js
@@ -0,0 +1,18 @@
+require(['xml_parser'], function(XmlParser) {
+  module('XmlParser', {
+    setup: function() {
+      xmlParser = new XmlParser();
+    }
+  });
+
+  test('findRecursive', function() {
+    expect(2);
+    var xml = '<root><up><sibling></sibling><loader>test value</loader></up></root>'
+    xmlParser.parseXML(xml);
+    var result = xmlParser.findRecursive('up:loader');
+    equal(result.text(), 'test value', 'finds nested element');
+    result = xmlParser.findRecursive('up:down');
+    equal(result, undefined, 'undefined when nested element doesnt exist');
+  });
+
+});
