33ea0db574f6379bedb16c9fd8d9ca939e1e82c8
Jason Madsen <jmadsen@instructure.com>
2014-02-24 09:16:48 -0700

    fix bug where passed allowedMediaTypes options not considered

--- test/k5_options.spec.js
+++ test/k5_options.spec.js
@@ -10,6 +10,17 @@ require(['./k5_options'], function(k5Options) {
     ok(k5Options);
   });
 
+  test('allowedMediaTypes', function() {
+    expect(3);
+    k5Options.setOptions({});
+    equal(k5Options.allowedMediaTypes[0], 'video', 'correct defaults');
+    equal(k5Options.allowedMediaTypes[1], 'audio', 'correct defaults');
+    k5Options.setOptions({
+      allowedMediaTypes: 'foo'
+    });
+    equal(k5Options.allowedMediaTypes, 'foo', 'passed options take precendence');
+  });
+
   test('handles entryDefaults', function() {
     expect(2);
     var opts = {
