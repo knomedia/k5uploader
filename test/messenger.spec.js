e47fcc4ac5ff1722902df890007817896fb3df06
Jason Madsen <jmadsen@instructure.com>
2014-02-24 13:37:29 -0700

    all ability to remove all callbacks for a specific named event

--- test/messenger.spec.js
+++ test/messenger.spec.js
@@ -39,4 +39,22 @@ require(['./messenger'], function(Messenger) {
     equal(result, false, 'dont add listeners when callback undefined');
   });
 
+  test('killAllListeners', function() {
+    var called = false;
+    var foo = 'foo';
+    function callback() {
+      called = true;
+    }
+    function otherCallback() {
+      foo = 'bar';
+    }
+    var messenger = new Messenger();
+    messenger.addEventListener('test.event', callback);
+    messenger.addEventListener('test.event', otherCallback);
+    messenger.killAllListeners('test.event');
+    messenger.dispatchEvent('test.event');
+    equal(called, false, 'removes callbacks');
+    equal(foo, 'foo', 'removes all callbacks');
+  });
+
 });
